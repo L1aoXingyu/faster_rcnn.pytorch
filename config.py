fd0ea9719f4050c17c9862c0c0fbac26e24991b4
xyliao <xyliao1993@qq.com>
2018-02-13 23:12:08 +0800

    change default configure

--- config.py
+++ config.py
@@ -15,27 +15,27 @@ class DefaultConfig(object):
     cityperson_test_img = './cityscape/leftImg8bit/val/'
     cityperson_test_annot = './gtBboxCityPersons/val/'
 
-    # result_file = 'cityperson.txt'
+    result_file = 'traffic_result.txt'
 
     # Visualization parameters.
-    # vis_dir = './cityperson_vis/'
+    vis_dir = './traffic_vis/'
     plot_freq = 500
 
     # Save frequency and directory.
-    # save_freq = 1
-    # save_file = './cityperson_save'
+    save_freq = 1
+    save_file = './traffic_save'
 
     min_size = 600
     max_size = 1000
     # Network hyperparameters.
-    ctx = 1
+    ctx = 0
     use_drop = False
     max_epoch = 14
-    lr = 1e-3
+    lr = 1e-4
     lr_decay = 0.1
     lr_decay_freq = 10
     weight_decay = 5e-4
-    use_adam = False
+    use_adam = True
 
     # Loss parameters.
     rpn_sigma = 3.
